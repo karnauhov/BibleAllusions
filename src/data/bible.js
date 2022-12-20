d0115a7efe83c13800566d51cf7d3eec0eec934e
Oleh Karnaukhov <karnauhov.oleg@gmail.com>
2022-12-20 08:59:13 +0100

    Removed weight from bible.js

--- src/data/bible.js
+++ src/data/bible.js
@@ -3,542 +3,434 @@ var BIBLE = {
     "books": [
         { 
             "book": "Gen",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Exod",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Lev",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Num",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Deut",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Josh",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Judg",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Ruth",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "1Sam",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "2Sam",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "1Kgs",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "2Kgs",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "1Chr",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "2Chr",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Ezra",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Neh",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Esth",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Job",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Ps",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Prov",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Eccl",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Song",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Isa",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Jer",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Lam",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Ezek",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Dan",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Hos",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Joel",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Amos",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Obad",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Jonah",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Mic",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Nah",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Hab",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Zeph",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Hag",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Zech",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Mal",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Matt",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Mark",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Luke",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "John",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Acts",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Rom",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "1Cor",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "2Cor",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Gal",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Eph",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Phil",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Col",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "1Thess",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "2Thess",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "1Tim",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "2Tim",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Titus",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Phlm",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Heb",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Jas",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "1Pet",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "2Pet",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "1John",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "2John",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "3John",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Jude",
-            "weight": 0,
             "chapters": []
         },
         { 
             "book": "Rev",
-            "weight": 0,
             "chapters": [
                 {
                     "num": 1,
-                    "weight": 0,
                     "verses": [
                         {
                             "num": 1,
-                            "weight": 0,
                             "words": [602, 2424, 5547, 3739, 1325, 846, 3588, 2316, 1166, 3588, 1401, 846, 3739, 1163, 1096, 1722, 5034, 2532, 4591, 649, 1223, 3588, 32, 846, 3588, 1401, 846, 2491]
                         },
                         {
                             "num": 2,
-                            "weight": 0,
                             "words": [3739, 3140, 3588, 3056, 3588, 2316, 2532, 3588, 3141, 2424, 5547, 3745, 1492]
                         },
                         {
                             "num": 3,
-                            "weight": 0,
                             "words": [3107, 3588, 314, 2532, 3588, 191, 3588, 3056, 3588, 4394, 2532, 5083, 3588, 1722, 846, 1125, 3588, 1063, 2540, 1451]
                         },
                         {
                             "num": 4,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 5,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 6,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 7,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 8,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 9,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 10,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 11,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 12,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 13,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 14,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 15,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 16,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 17,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 18,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 19,
-                            "weight": 0,
                             "words": []
                         },
                         {
                             "num": 20,
-                            "weight": 0,
                             "words": []
                         }
                     ]
                 },
                 {
                     "num": 2,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 3,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 4,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 5,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 6,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 7,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 8,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 9,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 10,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 11,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 12,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 13,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 14,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 15,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 16,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 17,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 18,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 19,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 20,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 21,
-                    "weight": 0,
                     "verses": []
                 },
                 {
                     "num": 22,
-                    "weight": 0,
                     "verses": []
                 }
             ]
